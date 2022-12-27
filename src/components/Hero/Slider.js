411c392d0e29ae790d0bcde87bcee0fbc43e4f65
Sayan <sayandey2106@gmail.com>
2022-12-27 13:54:44 +0530

    carousel added

---
+++ src/components/Hero/Slider.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import HeroSlider, {Slide} from "hero-slider"
+import './Hero.css'
+export default function Slider() {
+    const img1="https://images.livemint.com/img/2021/09/06/1600x900/BYJU's_1617611925125_1630937205472.jpg";
+    const img2="https://deshicompanies.com/wp-content/uploads/2020/10/BYJUs.jpg";
+    const img3="https://deshicompanies.com/wp-content/uploads/2020/10/BYJUs.jpg";
+  return (
+    <div>
+  {/* <AwesomeSlider animation="cubeAnimation">
+    <div data-src="https://images.livemint.com/img/2021/09/06/1600x900/BYJU's_1617611925125_1630937205472.jpg" />
+    <div data-src="https://images.moneycontrol.com/static-mcnews/2019/06/Byjus.jpg?impolicy=website&width=770&height=431" />
+    <div data-src="https://deshicompanies.com/wp-content/uploads/2020/10/BYJUs.jpg" />
+  </AwesomeSlider> */}
+
+<HeroSlider
+    slidingAnimation="left to right"
+    orientation="horizontal"
+    initialSlide={1}
+    autoplay="true"
+    autoplayDuration="2000"
+    style={{
+        backgroundColor:"rgba(0,0,0,0.33)"
+    }}
+    settings={{
+        slideDuration:250,
+        slideDelay:100,
+        shouldAutoplay:true,
+        shouldDisplayButtons:true,
+        autoplayDuration:2000,
+        
+        // height:"70vh"
+    }}
+    // height="70vh"
+>
+    <Slide
+     className='img-fluid'
+    background={{
+        backgroundImageSrc:img1,
+        backgroundAttachment:"fixed",
+        height:"70vh"
+    }}/>
+        <Slide
+        className='img-fluid'
+    background={{
+        backgroundImageSrc:img2 ,
+        backgroundAttachment:"fixed",
+        height:"70vh"
+    }}/>
+        <Slide
+         className='img-fluid'
+    background={{
+        backgroundImageSrc:img3,
+        backgroundAttachment:"fixed",
+        height:"70vh"
+    }}
+    
+   />
+     
+</HeroSlider>
+
+    </div>
+  )
+}
